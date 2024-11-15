b9dee527f99cbfc93470f820bbbe224a5e7db434
Nikola Stankovic <Nikola-Stankovic-1@gmail.com>
2024-11-15 20:21:08 +0100

    Kreiran JavaFX prikaz pocetne stranice

--- src/Glavni/Main.java
+++ src/Glavni/Main.java
@@ -1,5 +1,6 @@
 package Glavni;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,30 +11,24 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-
-        //Pokrece JavaFX aplikaciju
+        // Pokrece JavaFX aplikaciju
         launch(args);
-
-
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        // Testiranje JavaFX
-        VBox vBox = new VBox();
+        Label label = new Label("eServisnaKnjizica"); // Kreira labelu
+        label.setFont(new Font("Arial", 55));
 
-        Label label = new Label("FX test");
-        label.setFont(new Font("Arial", 20));
-        vBox.getChildren().add(label);
+        Button dugme = new Button("Prijavi se"); // Kreira dugme koje vodi na stranicu za prijavu
+        dugme.setStyle("-fx-font: 35 arial; -fx-base: #40c6de;"); //Stil dugmeta
 
-        Button dugme = new Button("Pritisni");
-        dugme.setStyle("-fx-font: 20 arial; -fx-base: #b6e7c9;");
-        vBox.getChildren().add(dugme);
+        VBox vBox = new VBox(35, label, dugme); // Kreira vBox sa parametrom koji postavlja razmak izmedju elemenata u vBox - u
+        vBox.setAlignment(Pos.CENTER); // Pozicionira vBox elemente na centar
 
         // Kreiranje scene i prikaz
-        Scene scene = new Scene(vBox, 400, 300);
-
+        Scene scene = new Scene(vBox, 700, 500);
         primaryStage.setTitle("Test JavaFX vBox");
         primaryStage.setScene(scene);
         primaryStage.show();
